767b565ccc27a49d1baa275f47d91c0a0a695850
preamp <pescobot@gmail.com>
2024-07-15 19:21:01 +0900

    title

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 export default function Banner() {
   return (
-    <section className='h-24 relative top-20'>
-      <div className='w-full h-48 bg-cover bg-banner opacity-40'></div>
+    <section className='h-20 relative top-20'>
+      <div className='w-full h-40 bg-cover bg-banner opacity-40'></div>
       <div className='absolute w-full top-14 text-center text-gray-50 drop-shadow-2xl'>
-        <p className='text-2xl mb-5 font-bold md:text-4xl'>
-          Reducing Retained Foreign body
+        <p className='text-2xl my-5 font-bold md:text-4xl'>
+          진료재료별 X-ray 검출 영상
         </p>
-        <p className='text-xl font-semibold md:text-2xl'>For Patient Safety</p>
       </div>
     </section>
   );
